29db502d28e4285df353f84d4871aa955da0c322
Santripta Sharma <santripta@gmail.com>
2020-07-15 14:02:40 +0530

    why

---
+++ content.js
@@ -0,0 +1,19 @@
+function Unstar()
+{
+    if(document.getElementsByClassName("loading-message").length == 0)
+    {
+        let homeStars = document.getElementsByClassName("muted-link");
+        for(let i = 2; i < homeStars.length; i++)
+        {
+            homeStars[i].innerHTML = "";
+        }
+
+        document.getElementsByClassName("social-count")[2].innerHTML = "0";
+        return;
+    }
+
+    setTimeout(Unstar, 200);
+}
+
+
+setTimeout(Unstar, 200);
